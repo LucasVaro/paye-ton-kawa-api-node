c05513b305eb04054647efe341fe75ca50b05ab0
Quentin Hasnaoui Moreau <68105158+qm-h@users.noreply.github.com>
2023-02-15 13:10:45 +0000

    add tests

--- src/tests/routes.spec.ts
+++ src/tests/routes.spec.ts
@@ -5,12 +5,18 @@ import index from '../routes/index';
 const app = express();
 app.use('/', index);
 
+<<<<<<< HEAD
 describe('Good Index Routes', function () {
   it('responds to /', async () => {
+=======
+describe('Good Home Routes', function () {
+  test('responds to /', async () => {
+>>>>>>> 48dedb3 (add tests)
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toEqual('Hello World!');
   });
+<<<<<<< HEAD
 
   it('not responds to /about', async () => {
     const res = await request(app).get('/about');
@@ -18,3 +24,6 @@ describe('Good Index Routes', function () {
     expect(res.text).not.toEqual('About Page');
   })
 });
+=======
+});
+>>>>>>> 48dedb3 (add tests)

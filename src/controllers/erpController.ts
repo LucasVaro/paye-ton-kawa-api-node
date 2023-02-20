b903fdbb0f994987f1a6a2317e97264963b14d28
Quentin Moreau <quentinmoreauh@hotmail.com>
2023-02-20 09:33:38 +0100

    rebase projetv2

--- src/controllers/erpController.ts
+++ src/controllers/erpController.ts
@@ -1,31 +1,31 @@
-import fetch from "node-fetch";
-import {BASE_URL} from "../models/constant";
+import { BASE_URL } from '../models/constant'
+import fetch from 'node-fetch'
 
 export default class ErpController {
     static getProduct(id: string): Promise<any> {
         return fetch(BASE_URL + `/products/${id}`)
             .then((res) => {
                 if (res.status !== 200) {
-                    throw new Error(res.statusText);
+                    throw new Error(res.statusText)
                 }
-                return res.json();
+                return res.json()
             })
             .then((data) => {
-                return data;
+                return data
             })
             .catch((err) => {
-                throw new Error(`Error fetching product ${err}`);
-            });
+                throw new Error(`Error fetching product ${err}`)
+            })
     }
 
     static getProducts(): Promise<any> {
         return fetch(BASE_URL + '/products')
             .then((res) => res.json())
             .then((data) => {
-                return data;
+                return data
             })
             .catch((err) => {
-                throw new Error(`Error fetching products ${err}`);
-            });
+                throw new Error(`Error fetching products ${err}`)
+            })
     }
 }

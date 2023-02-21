995064d492eb04a766c5ae2fa9e4300c3d471696
ClementBriquet <clembr99@gmail.com>
2023-02-21 10:35:42 +0100

    Add JWT

--- src/routes/crmRoutes.ts
+++ src/routes/crmRoutes.ts
@@ -9,8 +9,8 @@ router.get('/customers', CrmMiddleware.getAllCustomersMiddleware);
 
 router.get('/customers/:id', CrmMiddleware.getCustomerMiddleware);
 
-router.get('/orders/:customerId', CrmMiddleware.getCustomerOrdersMiddleware);
+router.get('/customers/:customerId/orders', CrmMiddleware.getCustomerOrdersMiddleware);
 
-router.get('/orders/:customerId/:orderId', CrmMiddleware.getCustomerOrderProductsMiddleware);
+router.get('/customers/:customerId/orders/:orderId', CrmMiddleware.getCustomerOrderProductsMiddleware);
 
 export default router;

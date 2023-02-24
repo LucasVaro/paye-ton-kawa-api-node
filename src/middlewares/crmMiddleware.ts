5c6101018eafe60d10ef6560fe6c9f16a3e106bc
Clément Briquet <clembr99@gmail.com>
2023-02-24 08:37:30 +0100

    Swagger upgrade (#10)
    * Update swagger for demo
    
    * Update swagger for demo
    
    * Add return type to swagger

--- src/middlewares/crmMiddleware.ts
+++ src/middlewares/crmMiddleware.ts
@@ -3,11 +3,23 @@ import CrmController from "../controllers/crmController";
 
 export default class CrmMiddleware {
     static getIndexMiddleware(req: Request, res: Response): void {
+        //#swagger.tags = ['CRM']
+        //#swagger.description = 'CRM router index'
+        /* #swagger.security = [{
+            "apiKeyAuth": []
+        }] */
         res.send('CRM Index');
     }
 
     static async getAllCustomersMiddleware(req: Request, res: Response, next: NextFunction): Promise<void> {
+        //#swagger.tags = ['CRM']
+        //#swagger.description = 'Endpoint to get all customers'
+        /* #swagger.security = [{
+            "apiKeyAuth": []
+        }] */
         try {
+            /* #swagger.responses[200] = {
+              schema: [{ "$ref": "#/definitions/Customer" }]} */
             const customers = await CrmController.getAllCustomers();
             res.status(200).json(customers);
         } catch (err) {
@@ -16,7 +28,14 @@ export default class CrmMiddleware {
     }
 
     static async getCustomerMiddleware(req: Request, res: Response, next: NextFunction): Promise<void> {
+        //#swagger.tags = ['CRM']
+        //#swagger.description = 'Endpoint to get a specific customer'
+        /* #swagger.security = [{
+            "apiKeyAuth": []
+        }] */
         try {
+            /* #swagger.responses[200] = {
+              schema: { "$ref": "#/definitions/Customer" }} */
             const id = req.params.id;
             const order = await CrmController.getCustomer(id);
             res.status(200).json(order);
@@ -26,7 +45,14 @@ export default class CrmMiddleware {
     }
 
     static async getCustomerOrdersMiddleware(req: Request, res: Response, next: NextFunction): Promise<void> {
+        //#swagger.tags = ['CRM']
+        //#swagger.description = 'Endpoint to get all orders of a customer'
+        /* #swagger.security = [{
+            "apiKeyAuth": []
+        }] */
         try {
+            /* #swagger.responses[200] = {
+              schema: [{ "$ref": "#/definitions/Order" }]} */
             const customerId = req.params.customerId;
             console.log(customerId);
             const orders = await CrmController.getCustomerOrders(customerId);
@@ -37,7 +63,14 @@ export default class CrmMiddleware {
     }
 
     static async getCustomerOrderProductsMiddleware(req: Request, res: Response, next: NextFunction): Promise<void> {
+        //#swagger.tags = ['CRM']
+        //#swagger.description = 'Endpoint to get all products of a customer's order'
+        /* #swagger.security = [{
+            "apiKeyAuth": []
+        }] */
         try {
+            /* #swagger.responses[200] = {
+              schema: [{ "$ref": "#/definitions/Product" }]} */
             const customerId = req.params.customerId;
             const orderId = req.params.orderId;
             const products = await CrmController.getCustomerOrderProducts(customerId, orderId);
--- src/middlewares/erpMiddleware.ts
+++ src/middlewares/erpMiddleware.ts
@@ -3,23 +3,42 @@ import ErpController from "../controllers/erpController";
 
 export default class ErpMiddleware {
     static getIndex(req: Request, res: Response): void {
+        //#swagger.tags = ['ERP']
+        //#swagger.description = 'ERP router index'
+        /* #swagger.security = [{
+            "apiKeyAuth": []
+        }] */
         res.send('ERP Controller');
     }
 
-    static async getProductMiddleware(req: Request, res: Response, next: NextFunction): Promise<void> {
+    static async getAllProductsMiddleware(req: Request, res: Response, next: NextFunction): Promise<void> {
+        //#swagger.tags = ['ERP']
+        //#swagger.description = 'Endpoint to get all products'
+        /* #swagger.security = [{
+            "apiKeyAuth": []
+        }] */
         try {
-            const id = req.params.id;
-            const product = await ErpController.getProduct(id);
-            res.status(200).json(product);
+            /* #swagger.responses[200] = {
+              schema: [{ "$ref": "#/definitions/Product" }]} */
+            const products = await ErpController.getProducts();
+            res.status(200).json(products);
         } catch (err) {
             next(err);
         }
     }
 
-    static async getAllProductsMiddleware(req: Request, res: Response, next: NextFunction): Promise<void> {
+    static async getProductMiddleware(req: Request, res: Response, next: NextFunction): Promise<void> {
+        //#swagger.tags = ['ERP']
+        //#swagger.description = 'Endpoint to get a specific product'
+        /* #swagger.security = [{
+            "apiKeyAuth": []
+        }] */
         try {
-            const products = await ErpController.getProducts();
-            res.status(200).json(products);
+            const id = req.params.id;
+            const product = await ErpController.getProduct(id);
+            /* #swagger.responses[200] = {
+              schema: { "$ref": "#/definitions/Product" }} */
+            res.status(200).json(product);
         } catch (err) {
             next(err);
         }
